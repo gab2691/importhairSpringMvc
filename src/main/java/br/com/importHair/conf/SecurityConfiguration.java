d823e9e7610b5fc01997ab83a22c423f92200f1a
gabriel <gabriel_bartholo_batista@hotmail.com>
2018-04-23 11:53:14 -0300

    teste

--- src/main/java/br/com/importHair/conf/SecurityConfiguration.java
+++ src/main/java/br/com/importHair/conf/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.requiresChannel().antMatchers("/").requiresSecure();
+		http.requiresChannel().antMatchers("/login**,/user**").requiresSecure();
 	    
 	}
 	
